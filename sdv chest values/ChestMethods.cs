5bbd24e0eea705eaff6ca3872ef9032044b41abd
uh chris <116903385+Chris-Kau@users.noreply.github.com>
2024-05-07 15:26:58 -0700

    added method to return total value

---
+++ sdv chest values/ChestMethods.cs
@@ -0,0 +1,26 @@
+﻿using StardewValley.Menus;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StardewValley;
+using StardewModdingAPI;
+namespace sdv_chest_values
+{
+    internal class ChestMethods
+    {
+        public static long GetTotalValue(StardewValley.Objects.Chest chest)
+        {
+            long total = 0;
+            foreach(Item i in chest.Items)
+            {
+                for(int j = 0; j < i.Stack; j++)
+                {
+                    total += i.sellToStorePrice();
+                }
+            }
+            return total;
+        }
+    }
+}
