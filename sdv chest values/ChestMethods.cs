61a5cdf5fea2cf2b7ca489b8ea31c2c40e69ec7a
Chris-Kau <uhchris04@gmail.com>
2024-05-08 00:39:24 -0700

    displays value of all items within chest upon mouse hovering over the chest.

--- sdv chest values/ChestMethods.cs
+++ sdv chest values/ChestMethods.cs
@@ -1,11 +1,11 @@
-﻿using StardewValley.Menus;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using StardewValley;
+﻿using System;
+using System.Security.AccessControl;
+using Microsoft.Xna.Framework;
+using sdv_chest_values;
 using StardewModdingAPI;
+using StardewModdingAPI.Events;
+using StardewModdingAPI.Utilities;
+using StardewValley;
 namespace sdv_chest_values
 {
     internal class ChestMethods
@@ -21,6 +21,23 @@ namespace sdv_chest_values
                 }
             }
             return total;
+        }
+
+        public static List<Vector2> GetChestLocations(GameLocation location)
+        {
+            List<Vector2> chestLocs = new List<Vector2>();
+            //Loops through each tile at the given location to check if there's a chest
+            for (int x = 0; x < location.map.Layers[0].LayerWidth; x++)
+            {
+                for (int y = 0; y < location.map.Layers[0].LayerHeight; y++)
+                {
+                    Vector2 temp = new Vector2(x, y);
+                    //checks to see if that tile is a valid tile, then check if the object on that tile is a Chest
+                    if (location.objects.ContainsKey(temp) && location.Objects[temp] is StardewValley.Objects.Chest)
+                        chestLocs.Add(new Vector2(x, y));
+                }
+            }
+            return chestLocs;
         }
     }
 }
