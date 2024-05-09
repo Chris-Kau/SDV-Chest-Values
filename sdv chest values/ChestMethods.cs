8b87915f74cba1c47ee50297afc1789e9d61fa81
Chris-Kau <uhchris04@gmail.com>
2024-05-09 00:04:14 -0700

    exclude prices who are 0

--- sdv chest values/ChestMethods.cs
+++ sdv chest values/ChestMethods.cs
@@ -15,29 +15,13 @@ namespace sdv_chest_values
             long total = 0;
             foreach(Item i in chest.Items)
             {
-                for(int j = 0; j < i.Stack; j++)
-                {
-                    total += i.sellToStorePrice();
-                }
+                if(i.sellToStorePrice() != 0)
+                    for(int j = 0; j < i.Stack; j++)
+                    {
+                        total += i.sellToStorePrice();
+                    }
             }
             return total;
-        }
-
-        public static List<Vector2> GetChestLocations(GameLocation location)
-        {
-            List<Vector2> chestLocs = new List<Vector2>();
-            //Loops through each tile at the given location to check if there's a chest
-            for (int x = 0; x < location.map.Layers[0].LayerWidth; x++)
-            {
-                for (int y = 0; y < location.map.Layers[0].LayerHeight; y++)
-                {
-                    Vector2 temp = new Vector2(x, y);
-                    //checks to see if that tile is a valid tile, then check if the object on that tile is a Chest
-                    if (location.objects.ContainsKey(temp) && location.Objects[temp] is StardewValley.Objects.Chest)
-                        chestLocs.Add(new Vector2(x, y));
-                }
-            }
-            return chestLocs;
         }
     }
 }
